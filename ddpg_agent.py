0bff9aa8c72c5c6f420b936597b1ebf39c77de11
Sriram Palutla <srirampalutla@Srirams-MacBook-Pro.local>
2019-07-24 17:51:40 -0700

    Final commit to Continuous_Control, Saved_Agent, ddpg_agent and checkpoint files

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -132,7 +132,7 @@ class Agent():
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
-    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
+    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.05):
         """Initialize parameters and noise process."""
         self.mu = mu * np.ones(size)
         self.theta = theta
